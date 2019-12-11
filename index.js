828db316fcd86621a8fade9ecd1e9e6490369244
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-12-11 10:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fi = (function() {
     },
 
     each: function(collection, callback) {
+<<<<<<< HEAD
+=======
+      let
+>>>>>>> 972e695ff8c364fcb7533a6ea93c99291e84f3f4
       for(var key in collection){
         callback(collection[key])
       }
@@ -28,6 +32,7 @@ const fi = (function() {
         result = collection.reduce(callback);
       }
       return result;
+<<<<<<< HEAD
     },
 
     find: function(collection, predicate) {
@@ -36,6 +41,96 @@ const fi = (function() {
           arr.push(collection[key])
       }
       return arr.find(predicate);
+    },
+    filter: function(collection,predicate){
+      return collection.filter(predicate);
+    },
+    size: function(collection){
+      let arr = [];
+      for(var key in collection){
+          arr.push(collection[key])
+      }
+      return arr.length;
+    },
+    first: function(collection, n) {
+      if(n != null){
+        return collection.slice(0,n);
+      }
+      return collection[0];
+    },
+    last:function(collection,n){
+      if(n != null){
+        return collection.slice(-n);
+      }
+      return collection[collection.length -1];
+    },
+    compact: function(collection) {
+      return collection.filter(Boolean);
+=======
+>>>>>>> 972e695ff8c364fcb7533a6ea93c99291e84f3f4
+    },
+    sortBy: function(collection,callback){
+      let arr = [...collection];
+      return arr.sort((a,b)=>{
+          if(typeof a === 'string'){
+              return a.localeCompare(b);
+          }
+          return callback(a) - callback(b)
+      });
+    },
+    flatten: function(collection, shallow) {
+      if(shallow == null){
+        function flatten(arr) {
+          return arr.reduce(function (flat, toFlatten) {
+            return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+          }, []);
+        }
+        return flatten(collection);
+      }
+      return collection.reduce((acc, val) => acc.concat(val), []);
+    },
+    uniq:function(collection,isSorted, callback){
+      if(isSorted != null){
+        const cond = collection.map(val=>callback(val)).filter((v,i,col)=>{
+            return col.indexOf(v) === i;
+        });
+
+<<<<<<< HEAD
+        let res = collection.map((x,i,col)=>{
+            // console.log(cond[cond.indexOf(callback(x))])
+            if(cond.includes(callback(x)) === true){
+                cond[cond.indexOf(callback(x))] = -1;
+                return x
+            }
+            return false
+
+        });
+        return res.filter(Boolean);
+    }
+      return collection.filter((v,i,col)=>{
+        return col.indexOf(v) === i;
+      });
+    },
+    keys: function(object) {
+      return Object.keys(object);
+    },
+    values: function(object) {
+      return Object.values(object);
+    },
+    giveMeMore: function(){
+      return true
+    },
+
+=======
+    find: function(collection, predicate) {
+      let chosen;
+      for(let i = 0; i < collection.length; i++){
+        if(collection[i] === predicate){
+          chosen = collection[i];
+          i += collection.length;
+        }
+      }
+      return chosen;
     },
     filter: function(collection,predicate){
       return collection.filter(predicate);
@@ -72,35 +167,14 @@ const fi = (function() {
       });
     },
     flatten: function(collection, shallow) {
-      if(shallow == null){
-        function flatten(arr) {
-          return arr.reduce(function (flat, toFlatten) {
-            return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
-          }, []);
-        }
-        return flatten(collection);
+      if(shallow != null){
+        return collection.flat(1);
       }
-      return collection.reduce((acc, val) => acc.concat(val), []);
+      return collection.flat(Infinity);
     },
     uniq:function(collection,isSorted, callback){
-      if(isSorted != null){
-        const cond = collection.map(val=>callback(val)).filter((v,i,col)=>{
-            return col.indexOf(v) === i;
-        });
-
-        let res = collection.map((x,i,col)=>{
-            // console.log(cond[cond.indexOf(callback(x))])
-            if(cond.includes(callback(x)) === true){
-                cond[cond.indexOf(callback(x))] = -1;
-                return x
-            }
-            return false
-
-        });
-        return res.filter(Boolean);
-    }
       return collection.filter((v,i,col)=>{
-        return col.indexOf(v) === i;
+          return col.indexOf(v) === i;
       });
     },
     keys: function(object) {
@@ -109,10 +183,7 @@ const fi = (function() {
     values: function(object) {
       return Object.values(object);
     },
-    giveMeMore: function(){
-      return true
-    },
-
+>>>>>>> 972e695ff8c364fcb7533a6ea93c99291e84f3f4
     functions:function(obj){
       let final =  [];
       for(var key in obj){
@@ -122,7 +193,15 @@ const fi = (function() {
       }
       return final.sort();
     },
+<<<<<<< HEAD
   };
 })();
+=======
+    giveMeMore: function() {
+          return true;
+      }
+  }
+})()
+>>>>>>> 972e695ff8c364fcb7533a6ea93c99291e84f3f4
 
 fi.libraryMethod()
